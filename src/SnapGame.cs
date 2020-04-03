a67a402833eeb98ceef72471fbbe3c2802696f0c
mau2318 <huyhoang2396@gmail.com>
2020-04-03 12:13:35 +1100

    add player hit with left and right shift, with score
    q
    aa

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -24,12 +24,27 @@ namespace CardGames
 			//Fetch the next batch of UI interaction
 			SwinGame.ProcessEvents();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
+			if (SwinGame.KeyTyped(KeyCode.vk_SPACE))
 			{
-				myGame.FlipNextCard ();
+				myGame.FlipNextCard();
+			}
+			if (myGame.IsStarted)
+			{
+				if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT) &&
+				SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+				{
+					//TODO: add sound effects
+				}
+				else if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT))
+				{
+					myGame.PlayerHit(0);
+				}
+				else if (SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+				{
+					myGame.PlayerHit(1);
+				}
 			}
 		}
-
 		/// <summary>
 		/// Draws the game to the Window.
 		/// </summary>
